4e2726a98540070949db00ac48ded96259d1eece
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2019-08-22 16:55:27 -0700

    Readded procfile and added a .gitignore

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client } = require("discord.js");
+const client = new Client();
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", message => {
+
   if (message.content === "ping") {
     message.reply("pong");
   }
