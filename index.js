8281d98ecb777dceac49544327f08e7253900d24
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2019-08-23 18:35:53 -0700

    Finished getting Soto assignment information

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { Client } = require("discord.js");
+const {
+  Client
+} = require("discord.js");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const pdf = require("pdf-parse");
+const moment = require("moment");
 
 const client = new Client();
 
@@ -10,30 +13,46 @@ client.on("message", async message => {
     switch (message.content.slice(3)) {
       case "soto":
         try {
-          const response = await axios.get("https://sites.google.com/site/sotomathpage/homework/calculus-ap-ib");
+          const response = await axios("https://sites.google.com/site/sotomathpage/homework/calculus-ap-ib");
           const $ = cheerio.load(response.data);
           let link;
           $("td.td-file").each(async (i, el) => {
             try {
               if ($(el).find("span.td-value").html().includes("Assignments")) {
-                console.log("sites.google.com" + $(el).find("span:nth-child(3) > a:nth-child(2)").attr("href"));
                 const doc = await axios({
-                  url: "https://jsonplaceholder.typicode.com/todos/1",
+                  url: "https://sites.google.com" + $(el).find("span:nth-child(3) > a:nth-child(2)").attr("href"),
                   method: "GET",
-                  responseType: "blob"
+                  responseType: "arraybuffer"
                 });
-                //const doc  = await axios.get("sites.google.com" + $(el).find("span:nth-child(3) > a:nth-child(2)").attr("href"));
-                //const a = await pdf("sites.google.com" + $(el).find("span:nth-child(3) > a:nth-child(2)").attr("href"));
-                console.log(doc);
-                return false;
+                const parsed = await pdf(Buffer.from(doc.data));
+                const text = parsed.text;
+                const matches = [...text.matchAll(/(\d{2})-(\w{3})-(\d{2})/g)];
+                let mostRecent = moment(matches[0][0], "DD-MMM-YYYY");
+                const indices = [matches[0].index];
+                matches.forEach(match => {
+                  const dateMoment = moment(match[0], "DD-MMM-YYYY");
+                  if (dateMoment.isBefore(moment()) && dateMoment.isAfter(mostRecent)) {
+                    mostRecent = dateMoment;
+                    indices.length = 0;
+                  }
+                  if (dateMoment.isSame(mostRecent)) {
+                    indices.push(match.index);
+                  }
+                });
+                indices.forEach(index => {
+                  const slicedText = text.slice(index);
+                  const section = slicedText.match(/\d+\.\d+/)[0];
+                  const assignment = slicedText.match(/P\..*?(P\.)[^\n]*/)[0];
+                  const problems = assignment.slice(assignment.indexOf("P.", assignment.indexOf("P.") + 1));
+                  message.channel.send(`**${section}** ${problems}`);
+                })
               }
-            }
-            catch (e) {
-              console.log(e);
+            } catch (e) {
+              message.channel.send(e + "Error getting homework information :(");
             }
           });
         } catch (e) {
-          message.channel.send(e + "Error getting homework information :(");
+          console.log(e);
         }
     }
   }
